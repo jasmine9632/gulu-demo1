095ffffcee46fbb9aafad80610e0032e2c8c1f1e
jasmine <360925221@qq.com>
2019-10-22 14:10:02 +0800

    验证iconPosition为left或right中的一个

--- src/button.vue
+++ src/button.vue
@@ -11,7 +11,18 @@
 <script>
 
 export default {
-  props: ['icon', 'iconPosition'],
+  // props: ['icon', 'iconPosition']
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      // 添加属性检查器
+      validator (value) {
+        return value === 'left' || value === 'right';
+      }
+    }
+  }
 }
 
 </script>
