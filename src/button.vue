89775e68a66f0e2393841d91992de9f43b48b966
jasmine <360925221@qq.com>
2019-10-22 19:25:47 +0800

    npm run test 测试失败

--- src/button.vue
+++ src/button.vue
@@ -7,9 +7,16 @@
     </div>
   </button>
 </template>
+
 <script>
+import Icon from './icon'
+
 
 export default {
+  components: {
+    'g-icon': Icon
+  },
+
   // props: ['icon', 'iconPosition']
   props: {
     icon: {},
