d734f46ff90ed2f2e986d91b0b72a80255ccc2dd
jasmine <360925221@qq.com>
2019-10-22 14:40:07 +0800

    添加loading状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-    <g-icon v-if="icon" :name="icon"></g-icon>
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -27,6 +28,14 @@ export default {
 
 
 <style lang="scss">
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -65,6 +74,9 @@ export default {
       margin-right: 0;
       margin-left: 0.1em;
     }
+  }
+  .loading {
+    animation: spin 2s infinite linear;
   }
 }
 </style>
