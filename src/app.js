b0419bdce0376c73b9112e1f4060a339d767a02f
jasmine <360925221@qq.com>
2019-10-25 14:35:36 +0800

    grid finished && add layout

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ import Button from "./button"
 import Icon from "./icon"
 import ButtonGroup from "./button-group"
 import Input from "./input"
+import Row from "./row"
+import Col from "./col"
+
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 
 new Vue({
@@ -18,9 +23,10 @@ new Vue({
         loading3: false,
         message: 'hi'
     },
+    created() {},
     methods: {
         inputChange(e) {
-            console.log(e.target.value)
+            console.log(e)
         }
     }
 
